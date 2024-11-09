5a7d0dcefa5eeae46e2a64a46a2b9f0f2c51150f
0mkar <0mkar@LAPTOP-TIJ138JI>
2024-11-09 12:09:50 +0530

    TL--->commit

--- GitProj01/src/main/java/nit/GitProj01/App.java
+++ GitProj01/src/main/java/nit/GitProj01/App.java
@@ -1,5 +1,7 @@
 package nit.GitProj01;
 
+import java.security.DomainCombiner;
+
 /**
  * Hello world!
  */
@@ -7,6 +9,11 @@ public class App {
 	public int sum(int a,int b) {
 		int c=a+b;
 		return c;
+		
+	}
+	public static void main(String[] args) {
+		App ap=new App();
+		System.out.println("sum is::"+ap.sum(10, 20));
 	}
     
 }
