9979c75783ca914a1649c64f466ff06ba8f8614b
0mkar <0mkar@LAPTOP-TIJ138JI>
2024-11-11 11:46:05 +0530

    dev1 ---commit

---
+++ GitProj01/src/main/java/com/nit/upi/UpiPayment.java
@@ -0,0 +1,11 @@
+package com.nit.upi;
+
+public class UpiPayment {
+	public String openAccount(String name,double balance) {
+		return "account created successfully";
+	}
+	
+	public String withdral(double ammount) {
+		return "transection sucessfull";
+	}
+}
