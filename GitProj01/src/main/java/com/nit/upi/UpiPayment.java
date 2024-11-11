bb638780c9c901f2507b636bd0403554bf79f5cb
0mkar <0mkar@LAPTOP-TIJ138JI>
2024-11-11 11:54:31 +0530

    TL---enhancing the code

--- GitProj01/src/main/java/com/nit/upi/UpiPayment.java
+++ GitProj01/src/main/java/com/nit/upi/UpiPayment.java
@@ -1,11 +1,17 @@
 package com.nit.upi;
 
+import java.util.Random;
+
 public class UpiPayment {
 	public String openAccount(String name,double balance) {
-		return "account created successfully";
+		double accNum=new Random().nextDouble(500000000);
+		return"account opened successfully "+accNum; 
 	}
 	
 	public String withdral(double ammount) {
 		return "transection sucessfull";
+	}
+	public String checkBalance(double accNum) {
+		return "your balance is"+new Random().nextDouble(50000000);
 	}
 }
